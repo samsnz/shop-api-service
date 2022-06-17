074f6ac06edec6848f279fbbfb61d6dae7cf4e6a
Sam Seneza <sseneza@gmail.com>
2022-06-17 12:53:26 +0200

    Added Swagger UI title and description

--- src/main/java/com/shop/service/ServiceApplication.java
+++ src/main/java/com/shop/service/ServiceApplication.java
@@ -3,7 +3,11 @@ package com.shop.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Soft Shop Service API Documentation", version = "1.0", description = "This service is a white-label solution to the existing retailer companies/hotels"))
 public class ServiceApplication {
 
 	public static void main(String[] args) {
